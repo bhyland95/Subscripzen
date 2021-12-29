f21e4989997d297b12585f273f8dc65503858fd2
Aaron Cox <aaroncox@Aarons-iMac.local>
2021-12-28 19:49:12 -0700

    css page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
+import React from 'react'
+import Home from './components/Home';
+import './App.css'
+
+
 function App() {
-    return (
-      <h1>FINAL PROJECT</h1>
-    );
-  }
-  
-  export default App;
+  return (
+    <div>
+      <main>
+        <h1>FINAL PROJECT</h1>
+        <Home></Home>
+      </main>
+    </div>
+  );
+}
+
+export default App;
