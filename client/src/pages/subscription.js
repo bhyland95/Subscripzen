e57e8d51c1e0648e704650d90da8a986088d539e
Michelle Black <86729483+mmagera84@users.noreply.github.com>
2021-12-29 10:21:18 -0700

    Update subscription.js
    updated-subscriptions

--- client/src/pages/subscription.js
+++ client/src/pages/subscription.js
@@ -30,3 +30,36 @@ const Subscription = () => {
         console.log(err);
       });
   };
+
+   const getSubscription = (e) => {
+    e.preventDefault();
+
+    axios
+      .get('/api/auth/getAllSubs')
+      .then((res) => {
+        const subs = res.data.subscriptions;
+        setSubscriptions(subs);
+        // for (let i = 0; i < subs.length; i++) {
+        //   const element = subs[i];
+        //   console.log(element);
+        // }
+        // setSubscription(element);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleInput = (event) => {
+    const url_link = event.target.value;
+    const urlStringify = JSON.stringify(url_link);
+    setUrl(urlStringify);
+  };
+
+  function deleteSubscription(id) {
+    API.deleteSubs(id)
+      .then((res) => loadSubscription())
+      .catch((err) => console.log(err));
+  }
+
+  const classes = useStyles();
