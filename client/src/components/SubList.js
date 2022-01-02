ffe63e380f2c74dd56d88a675fae554349c48f84
BH <blakehyland95@gmail.com>
2022-01-01 23:14:20 -0600

    worked on updated dates by one month once date passes

--- client/src/components/SubList.js
+++ client/src/components/SubList.js
@@ -1,14 +1,40 @@
 import React, {useEffect, useState} from 'react';
-
+import dateFormat from '../utils/dateFormat';
 
 
 const SubList = ({ subscriptions }) => {
 
 const [currentSubscriptions, setCurrentSubscriptions] = useState( [ ... subscriptions] )
 
+
+const today = new Date()
+
+
+
 function byDate(a, b) {
   return new Date(a.nextCharge) - new Date(b.nextCharge)
 }
+
+function beforeToday(a){
+  return (today - new Date(a.nextCharge)) >= 1
+}
+
+
+let filteredSubscriptions = [ ... subscriptions].filter(beforeToday)
+
+let newArray = [... filteredSubscriptions]
+
+for(var i = 0; i < filteredSubscriptions.length; i++){
+var timestamp = new Date(filteredSubscriptions[i].nextCharge)
+timestamp = dateFormat(timestamp.setMonth(timestamp.getMonth() + 1));
+
+
+console.log(timestamp)
+}
+
+
+console.log(filteredSubscriptions)
+
 
 
 
