75b5f62c67583270ce54e1dd40ebe9810b1f21d7
BH <blakehyland95@gmail.com>
2022-01-06 19:17:16 -0600

    small changes

--- client/src/pages/Subscriptions/subs.js
+++ client/src/pages/Subscriptions/subs.js
@@ -2,74 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Icon } from '@iconify/react';
 import "./subs.css"
-
+import { seeds } from '../../utils/seeds';
 const Subs = () => {
 
-    let seeds = [
-        {
-            id: 1,
-            icon: <Icon icon="cib:netflix" />,
-            name: "Netflix",
-            nextCharge: "",
-            amount: 13.99
-        },
-        {
-            id: 2,
-            icon: <Icon icon="cib:hulu" />,
-            name: "Hulu",
-            nextCharge: "",
-            amount: 6.99
-        },
-        {
-            id: 3,
-            icon: <Icon icon="ion:logo-xbox" />,
-            name: "Xbox",
-            nextCharge: "",
-            amount: 9.99
-        },
-        {
-            id: 4,
-            icon: <Icon icon="ant-design:apple-filled" />,
-            name: "Apple Music",
-            nextCharge: "",
-            amount: 9.99
-        },
-        {
-            id: 5,
-            icon: <Icon icon="akar-icons:spotify-fill" />,
-            name: "Spotify",
-            nextCharge: "",
-            amount: 9.99
-        },
-        {
-            id: 6,
-            icon: <Icon icon="simple-icons:prime" />,
-            name: "Amazon Prime",
-            nextCharge: "",
-            amount: 12.99
-        },
-        {
-            id: 7,
-            icon: <Icon icon="bx:bxl-audible" />,
-            name: "Audible",
-            nextCharge: "",
-            amount: 14.95
-        },
-        {
-            id: 8,
-            icon: <Icon icon="gg:gym" />,
-            name: "Gym",
-            nextCharge: "",
-            amount: 14.99
-        },
-        {
-            id: 9,
-            icon: <Icon icon="bi:playstation" />,
-            name: "PlayStation",
-            nextCharge: "",
-            amount: 9.99
-        },
-    ]
+   
 
 
 
---
+++ client/src/utils/seeds.js
@@ -0,0 +1,69 @@
+
+import { Icon } from '@iconify/react';
+import React from 'react';
+
+export let seeds = [
+    {
+        id: 1,
+        icon: <Icon icon="cib:netflix" />,
+        name: "Netflix",
+        nextCharge: "",
+        amount: 13.99
+    },
+    {
+        id: 2,
+        icon: <Icon icon="cib:hulu" />,
+        name: "Hulu",
+        nextCharge: "",
+        amount: 6.99
+    },
+    {
+        id: 3,
+        icon: <Icon icon="ion:logo-xbox" />,
+        name: "Xbox",
+        nextCharge: "",
+        amount: 9.99
+    },
+    {
+        id: 4,
+        icon: <Icon icon="ant-design:apple-filled" />,
+        name: "Apple Music",
+        nextCharge: "",
+        amount: 9.99
+    },
+    {
+        id: 5,
+        icon: <Icon icon="akar-icons:spotify-fill" />,
+        name: "Spotify",
+        nextCharge: "",
+        amount: 9.99
+    },
+    {
+        id: 6,
+        icon: <Icon icon="simple-icons:prime" />,
+        name: "Prime",
+        nextCharge: "",
+        amount: 12.99
+    },
+    {
+        id: 7,
+        icon: <Icon icon="bx:bxl-audible" />,
+        name: "Audible",
+        nextCharge: "",
+        amount: 14.95
+    },
+    {
+        id: 8,
+        icon: <Icon icon="gg:gym" />,
+        name: "Gym",
+        nextCharge: "",
+        amount: 14.99
+    },
+    {
+        id: 9,
+        icon: <Icon icon="bi:playstation" />,
+        name: "PlayStation",
+        nextCharge: "",
+        amount: 9.99
+    },
+]
